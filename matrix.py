9449cfb3c3c1df8aaf64d923f8d445a7a223d1d3
Fortune Soleil <fsoleil@stuy.edu>
2017-03-08 11:34:59 -0500

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -4,7 +4,7 @@ def make_translate( x, y, z ):
     return [[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]]
 
 def make_scale( x, y, z ):
-    pass
+    return [[x, 0, 0, 0], [0, y, 0, 0], [0, 0, z, 0], [0, 0, 0, 1]]
 
 def make_rotX( theta ):    
     pass
